b98c99cbeba87694146dbad26cb21d8c7b8a86e4
Davit Nakhutsrishvili <dtnakhutsrishvili@gmail.com>
2026-02-27 18:44:16 +0400

    feat: add popup-mobile

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -1,1 +1,12 @@
+const dialog = document.getElementById("donation-dialog");
 
+dialog.showModal();
+
+console.log(dialog)
+// openBtn.addEventListener("click", () => {
+//   dialog.showModal();
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   dialog.close();
+// });
