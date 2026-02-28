1dfa706fdc1b84aaf7f4a67c3f319a397fa945ef
Davit Nakhutsrishvili <dtnakhutsrishvili@gmail.com>
2026-02-28 16:18:17 +0400

    feat:add popup dialog component

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -1,12 +1,37 @@
 const dialog = document.getElementById("donation-dialog");
 
-dialog.showModal();
+const steps = document.querySelectorAll("#donation-dialog .step");
+let currentStep = 0;
 
-console.log(dialog)
-// openBtn.addEventListener("click", () => {
-//   dialog.showModal();
-// });
+function showStep(index) {
+  steps.forEach((step, i) => step.classList.toggle("active", i === index));
+}
 
-// closeBtn.addEventListener("click", () => {
-//   dialog.close();
-// });
+document.querySelectorAll(".next-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    if (currentStep < steps.length - 1) currentStep++;
+    showStep(currentStep);
+  });
+});
+
+document.querySelectorAll(".prev-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    if (currentStep > 0) currentStep--;
+    showStep(currentStep);
+  });
+});
+document.querySelectorAll(".donate").forEach(btn => {
+  btn.addEventListener("click", () => {
+    dialog.showModal();
+  });
+});
+
+
+dialog.addEventListener("click", (e) => {
+  if (e.target === dialog) {
+    dialog.close();
+  }
+});
+document.getElementsByClassName("complate-donation")[0].addEventListener("click", () => {
+  dialog.close();
+});
